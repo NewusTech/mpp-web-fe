4640f77068f8409605a38765deb47a3204a72c91
Arma Yoga Pratama <mgarmayogapratama@gmail.com>
2024-06-14 11:47:41 +0700

    Feat: protecting url layanan/booking-antrian, layanan/permohonan-layanan, riwayat, statistik, survey, dan profile

--- mpp-mobile/src/app/(root)/statistik/page.tsx
+++ mpp-mobile/src/app/(root)/statistik/page.tsx
@@ -1,5 +1,17 @@
+"use client";
 import StatisticsScreen from "@/components/statistics/statisticScreen";
+import Cookies from "js-cookie";
+import { redirect } from "next/navigation";
+import { useEffect } from "react";
 
 export default function StatisticsPage() {
+  const token = Cookies.get("Authorization");
+
+  useEffect(() => {
+    if (!token) {
+      redirect("/login");
+    }
+  }, []);
+
   return <StatisticsScreen />;
 }
