03e15364241e69648405121b1346109a811967da
Arma Yoga Pratama <mgarmayogapratama@gmail.com>
2024-06-27 10:41:27 +0700

    Feat: Memperbarui profile dan update profile

--- mpp-mobile/src/app/(root)/profile/[userId]/detail/[slug]/page.tsx
+++ mpp-mobile/src/app/(root)/profile/[userId]/detail/[slug]/page.tsx
@@ -25,6 +25,87 @@ import { useDebounce } from "@/hooks/useDebounce/useDebounce";
 import Cookies from "js-cookie";
 import { DesaType, KecamatanType, UpdateUserType } from "@/types/type";
 
+const genders = [
+  {
+    id: 1,
+    value: "Laki-laki",
+  },
+  {
+    id: 2,
+    value: "Perempuan",
+  },
+];
+
+const agamas = [
+  {
+    id: 1,
+    value: "Islam",
+  },
+  {
+    id: 2,
+    value: "Kristen",
+  },
+  {
+    id: 3,
+    value: "Katolik",
+  },
+  {
+    id: 4,
+    value: "Hindu",
+  },
+  {
+    id: 5,
+    value: "Buddha",
+  },
+  {
+    id: 6,
+    value: "Konghucu",
+  },
+];
+
+const pendidikans = [
+  {
+    id: 1,
+    value: "Tidak Sekolah",
+  },
+  {
+    id: 2,
+    value: "SD",
+  },
+  {
+    id: 3,
+    value: "SMP",
+  },
+  {
+    id: 4,
+    value: "SMA",
+  },
+  {
+    id: 5,
+    value: "Diploma 1",
+  },
+  {
+    id: 6,
+    value: "Diploma 2",
+  },
+  {
+    id: 7,
+    value: "Diploma 3",
+  },
+  {
+    id: 8,
+    value: "Strata 1 / Diploma 4",
+  },
+  {
+    id: 9,
+    value: "Strata 2",
+  },
+  {
+    id: 10,
+    value: "Strata 3",
+  },
+];
+
 export default function ProfileEditPage({
   params,
 }: {
@@ -35,8 +116,17 @@ export default function ProfileEditPage({
     null
   );
   const [selectedDesa, setSelectedDesa] = useState<number | null>(null);
+  const [selectedGender, setSelectedGender] = useState<number | null>(null);
+  const [selectedAgama, setSelectedAgama] = useState<number | null>(null);
+  const [selectedPendidikan, setSelectedPendidikan] = useState<number | null>(
+    null
+  );
   const [kecamatan, setKecamatan] = useState<KecamatanType[]>();
   const [desa, setDesa] = useState<DesaType[]>([]);
+  const [gender, setGender] = useState<{ id: number; value: string }[]>();
+  const [agama, setAgama] = useState<{ id: number; value: string }[]>();
+  const [pendidikan, setPendidikan] =
+    useState<{ id: number; value: string }[]>();
   const [searchKecamatan, setSearchKecamatan] = useState<string>("");
   const [searchDesa, setSearchDesa] = useState<string>("");
   const debounceSearchKecamatan = useDebounce(searchKecamatan);
@@ -46,6 +136,10 @@ export default function ProfileEditPage({
     email: "",
     telepon: "",
     nik: "",
+    gender: "",
+    agama: "",
+    pendidikan: "",
+    pekerjaan: "",
     kecamatan_id: "",
     desa_id: "",
     rt: "",
@@ -97,6 +191,19 @@ export default function ProfileEditPage({
       if (result.data.desa_id) {
         setSelectedDesa(result.data.desa_id);
       }
+
+      if (result.data.gender) {
+        setSelectedGender(result.data.gender);
+      }
+
+      if (result.data.agama) {
+        setSelectedAgama(result.data.agama);
+      }
+
+      if (result.data.pendidikan) {
+        setSelectedPendidikan(result.data.pendidikan);
+      }
+
       setIsDataFetched(true);
     } catch (error) {
       toast("Gagal mendapatkan data!");
@@ -105,6 +212,12 @@ export default function ProfileEditPage({
 
   useEffect(() => {
     fetchUser();
+  }, []);
+
+  useEffect(() => {
+    setGender(genders);
+    setAgama(agamas);
+    setPendidikan(pendidikans);
   }, []);
 
   const router = useRouter();
@@ -149,8 +262,35 @@ export default function ProfileEditPage({
     }
   }, [selectedDesa]);
 
+  useEffect(() => {
+    if (selectedGender !== null) {
+      setDetail((prevDetail) => ({
+        ...prevDetail,
+        gender: String(selectedGender),
+      }));
+    }
+  }, [selectedGender]);
+
+  useEffect(() => {
+    if (selectedAgama !== null) {
+      setDetail((prevDetail) => ({
+        ...prevDetail,
+        agama: String(selectedAgama),
+      }));
+    }
+  }, [selectedAgama]);
+
+  useEffect(() => {
+    if (selectedPendidikan !== null) {
+      setDetail((prevDetail) => ({
+        ...prevDetail,
+        pendidikan: String(selectedPendidikan),
+      }));
+    }
+  }, [selectedPendidikan]);
+
   return (
-    <div className="flex items-center justify-center pb-[36px] bg-primary-100 mt-[24px] md:mt-[32px] md:mb-0 md:pb-[56px]">
+    <div className="flex items-center justify-center pb-[36px] bg-primary-100 mt-[24px] md:mt-0 md:pt-6 md:mb-0 md:pb-[120px]">
       <div className="flex flex-col items-center w-full mx-[35px] md:mx-[200px]">
         <div className="flex self-start mb-[32px]">
           <h5 className="text-[20px] md:text-[26px] font-bold text-primary-800">
@@ -160,60 +300,193 @@ export default function ProfileEditPage({
 
         <div className="flex flex-col w-full bg-white rounded-2xl shadow-lg px-[15px] md:px-[75px] pt-[16px] md:pt-[32px]">
           <form onSubmit={onSubmit} className="flex flex-col w-full">
-            <div className="flex flex-col w-full mb-[4px] md:mb-4">
-              <ProfileEditInput
-                name="name"
-                types="text"
-                value={detail?.name || ""}
-                change={changeUser}
-                labelName="Nama Lengkap"
-                placeholder="Nama Lengkap"
-                classStyle="w-full pl-[16px] mt-1 h-[40px] border border-neutral-700 placeholder:opacity-[70%]"
-                labelStyle="text-[12px] text-neutral-900 font-semibold"
-              />
-            </div>
+            <div className="grid grid-cols-2 w-full my-[4px] md:gap-4">
+              <div className="flex flex-col w-full mb-[4px] md:mb-4">
+                <ProfileEditInput
+                  name="name"
+                  types="text"
+                  value={detail?.name || ""}
+                  change={changeUser}
+                  labelName="Nama Lengkap"
+                  placeholder="Nama Lengkap"
+                  classStyle="w-full pl-[16px] mt-1 h-[40px] border border-neutral-700 placeholder:opacity-[70%]"
+                  labelStyle="text-[12px] text-neutral-900 font-semibold"
+                />
+              </div>
 
-            <div className="flex flex-col w-full mb-[4px] md:mb-4">
-              <ProfileEditInput
-                name="nik"
-                types="number"
-                value={detail?.nik || ""}
-                change={changeUser}
-                labelName="Nama Lengkap"
-                placeholder="Nama Lengkap"
-                classStyle="w-full pl-[16px] mt-1 h-[40px] border border-neutral-700 placeholder:opacity-[70%]"
-                labelStyle="text-[12px] text-neutral-900 font-semibold"
-              />
-            </div>
+              <div className="flex flex-col w-full mb-[4px] md:mb-4">
+                <Label className="text-[12px] text-neutral-900 font-semibold">
+                  Jenis Kelamin
+                </Label>
 
-            <div className="flex flex-col w-full mb-[4px] md:mb-4">
-              <ProfileEditInput
-                name="telepon"
-                types="number"
-                value={detail?.telepon || ""}
-                change={changeUser}
-                labelName="Nama Lengkap"
-                placeholder="Nama Lengkap"
-                classStyle="w-full pl-[16px] mt-1 h-[40px] border border-neutral-700 placeholder:opacity-[70%]"
-                labelStyle="text-[12px] text-neutral-900 font-semibold"
-              />
-            </div>
-
-            <div className="flex flex-col w-full mb-[4px] md:mb-4">
-              <ProfileEditInput
-                name="email"
-                types="text"
-                value={detail?.email || ""}
-                change={changeUser}
-                labelName="Nama Lengkap"
-                placeholder="Nama Lengkap"
-                classStyle="w-full pl-[16px] mt-1 h-[40px] border border-neutral-700 placeholder:opacity-[70%]"
-                labelStyle="text-[12px] text-neutral-900 font-semibold"
-              />
+                <Select
+                  name="gender"
+                  value={selectedGender ? String(selectedGender) : undefined}
+                  onValueChange={(value) => {
+                    setSelectedGender(Number(value));
+                  }}>
+                  <SelectTrigger
+                    className={`${
+                      !selectedGender ? "opacity-70" : ""
+                    } border border-neutral-700 rounded-[50px] mt-1 bg-white md:h-[40px] pl-4 w-full mx-0 pr-0`}>
+                    <SelectValue
+                      placeholder="Pilih Jenis Kelamin"
+                      className={selectedGender ? "" : "placeholder:opacity-50"}
+                    />
+                  </SelectTrigger>
+                  <SelectContent className="w-full">
+                    {genders?.map(
+                      (gender: { id: number; value: string }, i: number) => (
+                        <SelectItem
+                          className="pr-none mt-2"
+                          key={i}
+                          value={String(gender.id)}>
+                          {gender.value}
+                        </SelectItem>
+                      )
+                    )}
+                  </SelectContent>
+                </Select>
+              </div>
             </div>
 
             <div className="grid grid-cols-2 w-full my-[4px] md:gap-4">
-              <div className="flex flex-col w-[120px] md:w-full">
+              <div className="flex flex-col w-full mb-[4px] md:mb-4">
+                <ProfileEditInput
+                  name="nik"
+                  types="number"
+                  value={detail?.nik || ""}
+                  change={changeUser}
+                  labelName="NIK"
+                  placeholder="NIK"
+                  classStyle="w-full pl-[16px] mt-1 h-[40px] border border-neutral-700 placeholder:opacity-[70%]"
+                  labelStyle="text-[12px] text-neutral-900 font-semibold"
+                />
+              </div>
+
+              <div className="flex flex-col w-full mb-[4px] md:mb-4">
+                <Label className="text-[12px] text-neutral-900 font-semibold">
+                  Agama
+                </Label>
+
+                <Select
+                  name="gender"
+                  value={selectedAgama ? String(selectedAgama) : undefined}
+                  onValueChange={(value) => {
+                    setSelectedAgama(Number(value));
+                  }}>
+                  <SelectTrigger
+                    className={`${
+                      !selectedAgama ? "opacity-70" : ""
+                    } border border-neutral-700 rounded-[50px] mt-1 bg-white md:h-[40px] pl-4 w-full mx-0 pr-0`}>
+                    <SelectValue
+                      placeholder="Pilih Jenis Kelamin"
+                      className={selectedAgama ? "" : "placeholder:opacity-50"}
+                    />
+                  </SelectTrigger>
+                  <SelectContent className="w-full">
+                    {agamas?.map(
+                      (agama: { id: number; value: string }, i: number) => (
+                        <SelectItem
+                          className="pr-none mt-2"
+                          key={i}
+                          value={String(agama.id)}>
+                          {agama.value}
+                        </SelectItem>
+                      )
+                    )}
+                  </SelectContent>
+                </Select>
+              </div>
+            </div>
+
+            <div className="grid grid-cols-2 w-full my-[4px] md:gap-4">
+              <div className="flex flex-col w-full mb-[4px] md:mb-4">
+                <ProfileEditInput
+                  name="telepon"
+                  types="number"
+                  value={detail?.telepon || ""}
+                  change={changeUser}
+                  labelName="Nomor Telepon"
+                  placeholder="Nomor Telepon"
+                  classStyle="w-full pl-[16px] mt-1 h-[40px] border border-neutral-700 placeholder:opacity-[70%]"
+                  labelStyle="text-[12px] text-neutral-900 font-semibold"
+                />
+              </div>
+
+              <div className="flex flex-col w-full mb-[4px] md:mb-4">
+                <Label className="text-[12px] text-neutral-900 font-semibold">
+                  Pendidikan
+                </Label>
+
+                <Select
+                  name="gender"
+                  value={
+                    selectedPendidikan ? String(selectedPendidikan) : undefined
+                  }
+                  onValueChange={(value) => {
+                    setSelectedPendidikan(Number(value));
+                  }}>
+                  <SelectTrigger
+                    className={`${
+                      !selectedPendidikan ? "opacity-70" : ""
+                    } border border-neutral-700 rounded-[50px] mt-1 bg-white md:h-[40px] pl-4 w-full mx-0 pr-0`}>
+                    <SelectValue
+                      placeholder="Pilih Jenis Kelamin"
+                      className={
+                        selectedPendidikan ? "" : "placeholder:opacity-50"
+                      }
+                    />
+                  </SelectTrigger>
+                  <SelectContent className="w-full">
+                    {pendidikans?.map(
+                      (
+                        pendidikan: { id: number; value: string },
+                        i: number
+                      ) => (
+                        <SelectItem
+                          className="pr-none mt-2"
+                          key={i}
+                          value={String(pendidikan.id)}>
+                          {pendidikan.value}
+                        </SelectItem>
+                      )
+                    )}
+                  </SelectContent>
+                </Select>
+              </div>
+            </div>
+
+            <div className="grid grid-cols-2 w-full my-[4px] md:gap-4">
+              <div className="flex flex-col w-[120px] mb-[4px] md:mb-4 md:w-full">
+                <ProfileEditInput
+                  name="email"
+                  types="text"
+                  value={detail?.email || ""}
+                  change={changeUser}
+                  labelName="Email"
+                  placeholder="Email"
+                  classStyle="w-full pl-[16px] mt-1 h-[40px] border border-neutral-700 placeholder:opacity-[70%]"
+                  labelStyle="text-[12px] text-neutral-900 font-semibold"
+                />
+              </div>
+
+              <div className="flex flex-col w-[120px] mb-[4px] md:mb-4 md:w-full">
+                <ProfileEditInput
+                  name="pekerjaan"
+                  types="text"
+                  value={detail?.pekerjaan || ""}
+                  change={changeUser}
+                  labelName="Pekerjaan"
+                  placeholder="Pekerjaan"
+                  classStyle="w-full pl-[16px] mt-1 h-[40px] border border-neutral-700 placeholder:opacity-[70%]"
+                  labelStyle="text-[12px] text-neutral-900 font-semibold"
+                />
+              </div>
+            </div>
+
+            <div className="grid grid-cols-2 w-full my-[4px] md:gap-4">
+              <div className="flex flex-col w-[120px] mb-[4px] md:mb-4 md:w-full">
                 <Label className="text-[12px] text-neutral-900 font-semibold">
                   Kecamatan
                 </Label>
@@ -262,7 +535,7 @@ export default function ProfileEditPage({
                 </Select>
               </div>
 
-              <div className="flex flex-col w-[120px] md:w-full md:self-end">
+              <div className="flex flex-col w-[120px] mb-[4px] md:mb-4 md:w-full">
                 <Label className="text-[12px] text-neutral-900 font-semibold">
                   Desa
                 </Label>
@@ -306,27 +579,27 @@ export default function ProfileEditPage({
             </div>
 
             <div className="grid grid-cols-2 w-full my-[4px] md:gap-4">
-              <div className="flex flex-col w-[120px] md:w-full">
+              <div className="flex flex-col w-[120px] mb-[4px] md:mb-4 md:w-full">
                 <ProfileEditInput
                   name="rt"
                   types="number"
                   value={detail?.rt || ""}
                   change={changeUser}
-                  labelName="Nama Lengkap"
-                  placeholder="Nama Lengkap"
+                  labelName="RT"
+                  placeholder="RT"
                   classStyle="w-full pl-[16px] mt-1 h-[40px] border border-neutral-700 placeholder:opacity-[70%]"
                   labelStyle="text-[12px] text-neutral-900 font-semibold"
                 />
               </div>
 
-              <div className="flex flex-col w-[120px] md:w-full">
+              <div className="flex flex-col w-[120px] mb-[4px] md:mb-4 md:w-full">
                 <ProfileEditInput
                   name="rw"
                   types="number"
                   value={detail?.rw || ""}
                   change={changeUser}
-                  labelName="Nama Lengkap"
-                  placeholder="Nama Lengkap"
+                  labelName="RW"
+                  placeholder="RW"
                   classStyle="w-full pl-[16px] mt-1 h-[40px] border border-neutral-700 placeholder:opacity-[70%]"
                   labelStyle="text-[12px] text-neutral-900 font-semibold"
                 />
@@ -334,7 +607,7 @@ export default function ProfileEditPage({
             </div>
 
             <div className="grid grid-cols-2 md:grid-cols-none w-full my-[4px] md:my-4">
-              <Label className="text-[12px] text-neutral-900 font-semibold">
+              <Label className="text-[12px] text-neutral-900 md:mb-4 font-semibold">
                 ALamat
               </Label>
 
@@ -343,7 +616,7 @@ export default function ProfileEditPage({
                 placeholder="Alamat"
                 value={detail?.alamat}
                 onChange={changeUser}
-                className="w-full rounded-3xl md:w-full h-[74px] md:h-[150px] text-[12px] placeholder:opacity-[70%]"
+                className="w-full rounded-3xl border border-neutral-700 md:w-full h-[74px] md:h-[150px] text-[12px] placeholder:opacity-[70%]"
               />
             </div>
 
