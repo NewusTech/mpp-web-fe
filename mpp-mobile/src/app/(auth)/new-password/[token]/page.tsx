f265fb90860c2da51b20b81897f312c14ef53188
Arma Yoga Pratama <armayoga@Armas-MacBook-Pro.local>
2024-07-27 12:18:43 +0700

    Feat: new password

--- mpp-mobile/src/app/(auth)/new-password/[token]/page.tsx
+++ mpp-mobile/src/app/(auth)/new-password/[token]/page.tsx
@@ -6,6 +6,8 @@ import Image from "next/legacy/image";
 import { Raleway } from "next/font/google";
 import Link from "next/link";
 import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { toast } from "sonner";
 
 const raleway = Raleway({
   subsets: ["latin"],
@@ -13,6 +15,43 @@ const raleway = Raleway({
 });
 
 export default function NewPassword({ params }: { params: { token: string } }) {
+  const [newPw, setNewPw] = useState({
+    newPassword: "",
+    confirmNewPassword: "",
+  });
+
+  const SubmitNewSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL_MPP}/user/reset/${params.token}`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(newPw),
+          cache: "no-store",
+        }
+      );
+
+      await response.json();
+
+      if (response.ok) {
+        toast("Silahkan cek email anda");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewPw({
+      ...newPw,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <section className="md:container md:mx-auto flex justify-center items-center bg-gradient-to-bl from-neutral-50 from-[-40%] via-primary-700 via-99% to-neutral-700 to-[120%] w-screen h-screen md:min-w-full">
       <div className="flex flex-col w-full gap-[10px] md:gap-0 items-center md:items-start justify-center md:justify-start rounded-xl bg-primary-200 mx-8 my-[70px] md:my-0 p-[32px] md:py-[70px] md:px-[120px] md:mx-[300px]">
@@ -49,11 +88,16 @@ export default function NewPassword({ params }: { params: { token: string } }) {
             </p>
           </div>
 
-          <form className="flex flex-col gap-2 w-full">
+          <form
+            onSubmit={SubmitNewSubmit}
+            className="flex flex-col gap-2 w-full">
             <div className="flex flex-col gap-y-4">
               <div className="flex rounded-[50px] bg-neutral-50 text-[14px] w-full h-[40px] font-normal border border-primary-700 placeholder:text-[14px] placeholder:text-neutral-700">
                 <Input
                   type="password"
+                  name="newPassword"
+                  value={newPw.newPassword}
+                  onChange={handleChange}
                   placeholder="Kata Sandi Baru"
                   className="rounded-[50px] border-none outline-none text-[14px] w-full h-[38px] pl-[15px] py-[10px] font-normal placeholder:text-[14px] focus:outline-none active:border-none focus:border-none active:outline-none placeholder:text-neutral-700"
                 />
@@ -64,6 +108,9 @@ export default function NewPassword({ params }: { params: { token: string } }) {
               <div className="flex rounded-[50px] bg-neutral-50 text-[14px] w-full h-[40px] font-normal border border-primary-700 placeholder:text-[14px] placeholder:text-neutral-700">
                 <Input
                   type="password"
+                  name="confirmNewPassword"
+                  value={newPw.confirmNewPassword}
+                  onChange={handleChange}
                   placeholder="Konfirmasi Kata Sandi Baru"
                   className="rounded-[50px] border-none outline-none text-[14px] w-full h-[38px] pl-[15px] py-[10px] font-normal placeholder:text-[14px] focus:outline-none active:border-none focus:border-none active:outline-none placeholder:text-neutral-700"
                 />
